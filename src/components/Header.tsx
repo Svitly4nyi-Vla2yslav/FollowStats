8906fb1cdbc2ee3879373aa5709825ca6609bf19
Svitly4nyi-Vla2yslav <126347711+Svitly4nyi-Vla2yslav@users.noreply.github.com>
2024-10-08 23:01:23 +0200

    add folder

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header: React.FC = () => {
+  return (
+    <header className='bg-blue-600 p-4 text-white'>
+        <h1 className='text-lg font-bold'>
+            Follow Stats
+        </h1>
+
+    </header>
+  )
+}
+
+export default Header
