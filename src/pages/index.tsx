a51ec24966b34b027ee2a4f8e40b37f1d35502d4
Svitly4nyi-Vla2yslav <126347711+Svitly4nyi-Vla2yslav@users.noreply.github.com>
2024-10-11 01:23:18 +0200

    add navigation back / onboarding

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,26 @@
 import Header from "@/components/Header";
+import { Metadata } from "next";
+import Link from "next/link";
 import React from "react";
+
+export const metadata: Metadata = {
+  title: "Follow Stats",
+  description: "Test Project",
+};
 
 export default function Home() {
   return (
     <div>
       <Header />
-      <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="flex items-center flex-col justify-center min-h-screen bg-gray-100">
         <h1 className="text-4xl font-bold text-blue-600">
           Hello, FollowStats!
         </h1>
+        <Link href="/onboarding" className="mt-4">
+          <span className="bg-yellow-400  mt-4 p-2 text-white rounded-lg ">
+            Get Started{" "}
+          </span>
+        </Link>
       </div>
     </div>
   );
--- src/pages/onboarding/index.tsx
+++ src/pages/onboarding/index.tsx
@@ -1,21 +1,43 @@
+import { useRouter } from "next/router";
 import React, { useState } from "react";
 
 const Onboarding: React.FC = () => {
   const [step, setStep] = useState(1);
+  const router = useRouter();
 
-  const nextStep = () => setStep(step + 1);
+  const nextStep = () => {
+    if (step < 3) {
+      setStep(step + 1);
+    } else {
+      router.push("/");
+    }
+  };
 
-  return  <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-  {step === 1 && <p>Welcome to FollowStats</p>}
-  {step === 2 && <p>Learn more about us!</p>}
-  {step === 3 && <p>Enter your Instagram username</p>}
-  <button
-    onClick={nextStep}
-    className="mt-4 p-2 bg-blue-500 text-white rounded"
-  >
-    Next
-  </button>
-</div>
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      {step === 1 && (
+        <p className="text-4xl text-center font-bold text-blue-600">
+          Welcome to FollowStats
+        </p>
+      )}
+      {step === 2 && (
+        <p className="text-4xl  text-center font-bold text-blue-600">
+          Learn more about us!
+        </p>
+      )}
+      {step === 3 && (
+        <p className="text-4xl  text-center font-bold text-blue-600">
+          Enter your Instagram username
+        </p>
+      )}
+      <button
+        onClick={nextStep}
+        className="bg-yellow-400 mt-4 p-2 text-white rounded-lg "
+      >
+        {step < 3 ? "Next" : "Finish"}
+      </button>
+    </div>
+  );
 };
 
 export default Onboarding;
