fb42ab87de5f174d89b3db9897292656e161d6d1
Svitly4nyi-Vla2yslav <126347711+Svitly4nyi-Vla2yslav@users.noreply.github.com>
2024-10-11 11:36:19 +0200

    add axios

--- src/pages/onboarding/index.tsx
+++ src/pages/onboarding/index.tsx
@@ -1,15 +1,29 @@
 import { useRouter } from "next/router";
 import React, { useState } from "react";
+import axios from "axios";
 
 const Onboarding: React.FC = () => {
   const [step, setStep] = useState(1);
+  const [username, setUsername] = useState("");
   const router = useRouter();
+  const [loading, setLoading] = useState(false);
 
-  const nextStep = () => {
+  const nextStep = async () => {
     if (step < 3) {
       setStep(step + 1);
     } else {
-      router.push("/");
+      setLoading(true);
+      try {
+        const response = await axios.get(
+        `https://jsonplaceholder.typicode.com/users/1`
+        );
+        console.log("user data:", response.data);
+        router.push("/");
+      } catch (error) {
+        console.error("Error fetching user data :", error);
+      } finally {
+        setLoading(false);
+      }
     }
   };
 
@@ -26,15 +40,23 @@ const Onboarding: React.FC = () => {
         </p>
       )}
       {step === 3 && (
-        <p className="text-4xl  text-center font-bold text-blue-600">
-          Enter your Instagram username
-        </p>
+        <div className="flex flex-col items-center text-3xl  text-center font-bold text-blue-600">
+          <p>Enter your Instagram username:</p>
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            className="mt-2 p-2 border text-xs border-blue-500 rounded-lg"
+            placeholder="Instagram username"
+          />
+        </div>
       )}
       <button
         onClick={nextStep}
         className="bg-yellow-400 mt-4 p-2 text-white rounded-lg "
+        disabled={loading}
       >
-        {step < 3 ? "Next" : "Finish"}
+        {loading ? "Loading...🧻" : step < 3 ? "Next" : "Finish"}
       </button>
     </div>
   );
