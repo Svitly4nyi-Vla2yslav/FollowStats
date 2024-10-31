4c2ebc8fd403c61f700e73ad9fbcd5b40660f548
Svitly4nyi-Vla2yslav <126347711+Svitly4nyi-Vla2yslav@users.noreply.github.com>
2024-10-31 21:20:52 +0100

    change _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,21 +4,34 @@ import Head from "next/head";
 import React from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return  <>
-  <Head>
-    {/* Основні метадані для Open Graph */}
-    <meta property="og:title" content="Follow Stats" />
-    <meta property="og:description" content="Слідкуйте за статистикою своїх соцмереж" />
-    <meta property="og:image" content="https://follow-stats.netlify.app/assets/tempImageryDmsK.png" />
-    <meta property="og:url" content="https://follow-stats.netlify.app/" />
-    <meta property="og:type" content="website" />
+  return (
+    <>
+      <Head>
+        <meta property="og:title" content="Follow Stats" />
+        <meta
+          property="og:description"
+          content="Analyze your Instagram followers with Follow Stats!"
+        />
+        <meta
+          property="og:image"
+          content="https://follow-stats.netlify.app/tempImageryDmsK.png"
+        />
+        <meta property="og:url" content="https://follow-stats.netlify.app" />
+        <meta property="og:type" content="website" />
 
-    {/* Twitter Card метадані */}
-    <meta name="twitter:card" content="summary_large_image" />
-    <meta name="twitter:title" content="Follow Stats" />
-    <meta name="twitter:description" content="Слідкуйте за статистикою своїх соцмереж" />
-    <meta name="twitter:image" content="https://follow-stats.netlify.app/assets/tempImageryDmsK.png" />
-  </Head>
-  <Component {...pageProps} />
-</>;
+        {/* Twitter Card метадані */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Follow Stats" />
+        <meta
+          name="twitter:description"
+          content="Analyze your Instagram followers with Follow Stats!"
+        />
+        <meta
+          name="twitter:image"
+          content="https://follow-stats.netlify.app/tempImageryDmsK.png"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
