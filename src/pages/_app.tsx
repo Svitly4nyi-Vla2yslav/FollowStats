cddac363a6c2b1ab3dfa19c8e3f8377677c485d5
Svitly4nyi-Vla2yslav <126347711+Svitly4nyi-Vla2yslav@users.noreply.github.com>
2024-10-31 20:30:08 +0100

    add metada Open Graph

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,24 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import React from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return  <>
+  <Head>
+    {/* Основні метадані для Open Graph */}
+    <meta property="og:title" content="Follow Stats" />
+    <meta property="og:description" content="Слідкуйте за статистикою своїх соцмереж" />
+    <meta property="og:image" content="https://follow-stats.netlify.app/assets/tempImageryDmsK.png" />
+    <meta property="og:url" content="https://follow-stats.netlify.app/" />
+    <meta property="og:type" content="website" />
+
+    {/* Twitter Card метадані */}
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:title" content="Follow Stats" />
+    <meta name="twitter:description" content="Слідкуйте за статистикою своїх соцмереж" />
+    <meta name="twitter:image" content="https://follow-stats.netlify.app/assets/tempImageryDmsK.png" />
+  </Head>
+  <Component {...pageProps} />
+</>;
 }
